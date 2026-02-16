45777c9316d15ebc749f32da7022e1cc0a58cd6d
YMolodyko <VTB4081847@region.vtb.ru>
2026-02-16 22:24:45 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,31 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(value string) (string, error) {
+	builder := strings.Builder{}
+	previousLetter := ""
+	for _, value := range value {
+		if letterCount, err := strconv.Atoi(string(value)); err == nil {
+			if previousLetter == "" {
+				return "", ErrInvalidString
+			}
+			builder.WriteString(strings.Repeat(previousLetter, letterCount))
+			previousLetter = ""
+			continue
+		}
+		if previousLetter != "" {
+			builder.WriteString(previousLetter)
+		}
+		previousLetter = string(value)
+	}
+	if previousLetter != "" {
+		builder.WriteString(previousLetter)
+	}
+	return builder.String(), nil
 }
